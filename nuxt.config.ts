d84d8d119ab6ef2268f090c37554b5c0db737e9f
Avetius <avet.sargsyan@gmail.com>
2025-09-29 19:39:31 -0700

    vuetify added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,65 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
+  pages: true,
+  ssr: false,
+  devtools: { enabled: false },
+  css: [
+    'vuetify/styles',
+  ],
+  plugins: ['~/plugins/vuetify.js'],
+  components: true,
+  build: {
+    transpile: ['vuetify'],
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+    '@nuxt/ui',
+    'vuetify-nuxt-module'
+  ],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    }
+  }
 })
+    // '@mdi/font/css/materialdesignicons.min.css',
+// 
+// export default defineNuxtConfig({
+//   pages: true,
+//   ssr: false,
+//   devtools: { enabled: false },
+//   css: [
+//     'vuetify/styles',
+//     '@mdi/font/css/materialdesignicons.min.css',
+//   ],
+//   plugins: ['~/plugins/vuetify.js'],
+//   components: true,
+//   build: {
+//     transpile: ['vuetify'],
+//   },
+// })
+
+// Nuxt config file
+// import { defineNuxtConfig } from 'nuxt/config'
+
+// export default defineNuxtConfig({
+//   modules: [
+//     'vuetify-nuxt-module'
+//   ],
+//   vuetify: {
+//     moduleOptions: {
+//       /* module specific options */
+//     },
+//     vuetifyOptions: {
+//       /* vuetify options */
+//     }
+//   }
+// })
